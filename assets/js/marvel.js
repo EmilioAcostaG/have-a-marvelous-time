da247357605caaa4e6f97f21466a70f5056a8e45
MakeithappenMike <omichaelaaron@gmail.com>
2022-08-15 20:02:28 -0400

    got some data pulling

--- assets/js/marvel.js
+++ assets/js/marvel.js
@@ -1,5 +1,7 @@
+var character = "thor";
+$(".profileTitle").append(character);
 function getCharacter() {
-    var characterUrl = "https://gateway.marvel.com/v1/public/characters?hash=5ad57e2950ad170a8c45b38ddb6b3b01&ts=string&name=beast&apikey=72b7c45c60389c825df0845f4afd3c85";
+    var characterUrl = "https://gateway.marvel.com/v1/public/characters?hash=5ad57e2950ad170a8c45b38ddb6b3b01&ts=string&name=" + character + "&apikey=72b7c45c60389c825df0845f4afd3c85";
 
     return fetch(characterUrl)
     .then((characterResponse) => {
@@ -7,9 +9,15 @@ function getCharacter() {
     })
     .then((characterResponse) => {
         var code = characterResponse.code;
+        var name = characterResponse.data.results[0].name;
+        var numComics = characterResponse.data.results[0].comics.available;
+        var description = characterResponse.data.results[0].description;
+
+        console.log(characterResponse);
         console.log("Code:", code);
-        console.log("Code:", code);
-        console.log("Code:", code);
+        console.log("Name:", name);
+        console.log("#Comics:", numComics);
+        console.log("Desc:", description);
 
     })
     .catch(error => console.log('error', error));

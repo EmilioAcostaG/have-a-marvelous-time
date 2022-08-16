8a25947f9e8d5bb73de85d09bdbcc1fc91aacb11
MakeithappenMike <omichaelaaron@gmail.com>
2022-08-16 06:41:08 -0400

    added click functionality to change city button:

--- assets/js/characters.js
+++ assets/js/characters.js
@@ -3,38 +3,45 @@ var favMeal;
 var hobbies;
 var somethingToEat;
 var somethingToDo;
+// var changeCityButton = $(".changeCityButton");
 
 function getCharacterInfo() {
     var characterInfoUrl = "assets/json/characters.json";
-    console.log("Getting Characters");
+    // console.log("Getting Characters");
 
     return fetch(characterInfoUrl)
     .then((characterInfoResponse) => {
         return characterInfoResponse.json();
     })
     .then((characterInfoResponse) => {
-        console.log(characterInfoResponse);
+        // console.log(characterInfoResponse);
         favThing = characterInfoResponse[3]["Character"];
         favMeal = characterInfoResponse[3]["Food"];
         hobbies = characterInfoResponse[3]["Hobby"];
         somethingToEat = characterInfoResponse[3]["Place"];
         somethingToDo = characterInfoResponse[3]["Restaraunts"];
 
-        console.log("Fav Thing:", favThing);
-        console.log("Fav Meal:", favMeal);
-        console.log("Hobbies:", hobbies);
-        console.log("To Eat:", somethingToEat);
-        console.log("To Do:", somethingToDo);
+        // console.log("Fav Thing:", favThing);
+        // console.log("Fav Meal:", favMeal);
+        // console.log("Hobbies:", hobbies);
+        // console.log("To Eat:", somethingToEat);
+        // console.log("To Do:", somethingToDo);
 
         $(".favThing").html(favThing);
         $(".favMeal").html(favMeal);
         $(".hobbies").html(hobbies);
         $(".toEat").html(somethingToEat);
         $(".toDo").html(somethingToDo);
+
+        $(document).ready(function(){
+            $(".changeCityButton").click(function (event) {
+                console.log(event);
+                console.log("click");
+            });  
+        });
         
-
-
     })
     .catch(error => console.log('error', error));
       
 } getCharacterInfo();
+
--- assets/js/marvel.js
+++ assets/js/marvel.js
@@ -1,35 +1,28 @@
-var character = "thor";
-var code;
-var name;
+var statusCode;
+var characterName = "thor";
 var numComics;
-var description;
+var characterDescription;
 
 function getCharacter() {
-    var characterUrl = "https://gateway.marvel.com/v1/public/characters?hash=5ad57e2950ad170a8c45b38ddb6b3b01&ts=string&name=" + character + "&apikey=72b7c45c60389c825df0845f4afd3c85";
+    var characterUrl = "https://gateway.marvel.com/v1/public/characters?hash=5ad57e2950ad170a8c45b38ddb6b3b01&ts=string&name=" + characterName + "&apikey=72b7c45c60389c825df0845f4afd3c85";
 
     return fetch(characterUrl)
     .then((characterResponse) => {
         return characterResponse.json();
     })
     .then((characterResponse) => {
-        var code = characterResponse.code;
-        var name = characterResponse.data.results[0].name;
+        // var code = characterResponse.code;
+        var characterName = characterResponse.data.results[0].name;
         var numComics = characterResponse.data.results[0].comics.available;
-        var description = characterResponse.data.results[0].description;
+        var characterDescription = characterResponse.data.results[0].description;
 
         // console.log(characterResponse);
-        console.log("Code:", code);
-        console.log("Name:", name);
+        // console.log("Code:", statusCode);
+        console.log("Name:", characterName);
         console.log("#Comics:", numComics);
-        console.log("Desc:", description);
-        $(".profileTitle").html(character.toUpperCase());
-var characerDescription = "This is a description";
-$(".description").html(description);
-var numComics = "Number of Comics";
-$(".description").html(description);
-var description = "This is a description";
-$(".description").html(description);
-
+        console.log("Desc:", characterDescription);
+        $(".profileTitle").html(characterName.toUpperCase());
+        $(".characterDescription").html(characterDescription);
     })
     .catch(error => console.log('error', error));
       

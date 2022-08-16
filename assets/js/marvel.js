73f9090c958ccb91871fbf4eaa98d8e95580ecce
MakeithappenMike <omichaelaaron@gmail.com>
2022-08-15 20:20:31 -0400

    adding Bulma

--- assets/js/marvel.js
+++ assets/js/marvel.js
@@ -1,5 +1,9 @@
 var character = "thor";
-$(".profileTitle").append(character);
+var code;
+var name;
+var numComics;
+var description;
+
 function getCharacter() {
     var characterUrl = "https://gateway.marvel.com/v1/public/characters?hash=5ad57e2950ad170a8c45b38ddb6b3b01&ts=string&name=" + character + "&apikey=72b7c45c60389c825df0845f4afd3c85";
 
@@ -18,6 +22,13 @@ function getCharacter() {
         console.log("Name:", name);
         console.log("#Comics:", numComics);
         console.log("Desc:", description);
+        $(".profileTitle").html(character.toUpperCase());
+var characerDescription = "This is a description";
+$(".description").html(description);
+var numComics = "Number of Comics";
+$(".description").html(description);
+var description = "This is a description";
+$(".description").html(description);
 
     })
     .catch(error => console.log('error', error));

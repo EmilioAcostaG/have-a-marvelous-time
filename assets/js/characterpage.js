ada2f0e16d4496d92fa0b208549c2db28407bdce
MakeithappenMike <omichaelaaron@gmail.com>
2022-08-17 19:44:05 -0400

    character-page

---
+++ assets/js/characterpage.js
@@ -0,0 +1,18 @@
+// function showProfile() {
+//     $(".changeCityButton").click(function (event) {
+//         console.log("click");
+//     }); showProfile();
+// };
+
+$(".changeCityButton").click(function (event) {
+    console.log("click");
+});
+
+$(".characterBox").click(function (event) {
+    console.log("click");
+});
+
+$(".characterBox").click(function (event) {
+    var characterClicked = event.currentTarget.classList[1];
+    console.log(characterClicked);
+});

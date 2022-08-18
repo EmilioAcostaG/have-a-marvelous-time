0fe3c8d00be19ea554a12ceb9f0c47e5bb198d34
MakeithappenMike <omichaelaaron@gmail.com>
2022-08-17 21:40:39 -0400

    working on redirect

--- assets/js/characterpage.js
+++ assets/js/characterpage.js
@@ -15,15 +15,10 @@ $(".characterBox").click(function (event) {
 $(".characterBox").click(function (event) {
     var characterClicked = event.currentTarget.classList[1];
     console.log(characterClicked);
-<<<<<<< HEAD
-    window.location.replace("./characters.html");
-});
-=======
-    window.location.replace("./profile");
+    window.location.replace("/profile");
 });
 
 // $(".profileImage").attr("src", profileImage);
 // console.log("image url", profileImage);
 
 // $(".profileImage").css("background-image", "url(" + profileImage + ")");
->>>>>>> 6bcf57591c2e979dbdd4e191ee930c77f702995b

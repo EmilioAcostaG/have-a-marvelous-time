2d8894117c0350c32fad0f77f9ae7712c935d167
MakeithappenMike <omichaelaaron@gmail.com>
2022-08-18 06:33:53 -0400

    consolodated character.js into style.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -83,17 +83,64 @@ var changeCityBtn = $('#changeCity');
 console.log("city", city);
 changeCityBtn.val(city);
 
+// Handle change city button on character and profile pages
 $(".changeCityButton").click(function (event) {
     console.log("click");
 });
 
+// Handle click function on character images
 $(".characterBox").click(function (event) {
     console.log("click");
 });
 
+// Handle redirect to profile on click
 $(".characterBox").click(function (event) {
     var characterClicked = event.currentTarget.classList[1];
     console.log(characterClicked);
     window.location.replace("./profile.html");
 });
+
+// Get character info from Characters.json
+var favThing;
+var favMeal;
+var hobbies;
+var somethingToEat;
+var somethingToDo;
+var readMore;
+// var city = "{CITY}"
+
+function getCharacterInfo() {
+    var characterInfoUrl = "assets/json/characters.json";
+    // console.log("Getting Characters");
+
+    return fetch(characterInfoUrl)
+    .then((characterInfoResponse) => {
+        return characterInfoResponse.json();
+    })
+    .then((characterInfoResponse) => {
+        // console.log(characterInfoResponse);
+        favThing = characterInfoResponse[3]["Character"];
+        favMeal = characterInfoResponse[3]["Food"];
+        hobbies = characterInfoResponse[3]["Hobby"];
+        somethingToEat = characterInfoResponse[3]["Restaraunts"];
+        somethingToDo = characterInfoResponse[3]["Place"];
+        readMore = characterInfoResponse[3]["ReadMore"];
+        // console.log(somethingToEat.join(', '));
+
+        $(".city").html(city);
+        $(".favThing").html(favThing);
+        $(".favMeal").html(favMeal);
+        $(".hobbies").html(hobbies.join(', '));
+        $(".toEat").html(somethingToEat.join(', '));
+        $(".toDo").html(somethingToDo.join(', '));
+        $(".readMore").attr("href", readMore)
+        $(".changeCityButton").click(function (event) {
+            console.log("click");
+        });
+        
+
+    })
+    .catch(error => console.log('error', error));
+      
+} getCharacterInfo();
 
